6aae8090eaf440f0084d2ce9193aa77cb102d4e1
ryanascherr <ryanascherr@gmail.com>
2022-05-06 13:38:21 -0400

    Further styled game cards. Began working on dropdown nav. Added some media queries.

--- script.js
+++ script.js
@@ -28,11 +28,30 @@ const gamesList = [
         img: "./img/the-crew.png",
         genre: "Trick-Taking Game",
         favorite: true
+    },
+    {
+        name: "Skull",
+        minPlayers: 3,
+        maxPlayers: 6,
+        playTime: 20,
+        description: "Skull & Roses is the quintessence of bluffing, a game in which everything is played in the players' heads. Each player plays a face-down card, then each player in turn adds one more card - until someone feels safe enough to state that he can turn a number of cards face up and get only roses. Other players can then overbid him, saying they can turn even more cards face up. The highest bidder must then turn that number of cards face up, starting with his own. If he shows only roses, he wins; if he reveals a skull, he loses, placing one of his cards out of play. Two successful challenges wins the game. Skull & Roses is not a game of luck; it's a game of poker face and meeting eyes.",
+        img: "./img/skull.png",
+        genre: "Social Deduction",
+        favorite: true
+    },
+    {
+        name: "Skull",
+        minPlayers: 3,
+        maxPlayers: 10,
+        playTime: 10,
+        description: "One Night Ultimate Werewolf is a fast-paced game where everyone is dealt a secret role. Many of these roles have special abilities that will aid that player in gaining information.  In the course of only one night and the following morning, the players will determine who among them is a werewolf...hopefully.",
+        img: "./img/one-night-ultimate-werewolf.png",
+        genre: "Social Deduction",
+        favorite: false
     }
 ]
 
 $.each(gamesList, function() {
-    console.log(this.name);
     if (this.favorite) {
         $(".favorites-container").append(
         `<div class="game">
@@ -47,9 +66,31 @@ $.each(gamesList, function() {
         <hr>
         </div>`)
     }
-    // $("body").append(`<img src="${this.img}">`);
-})
+});
+
+$.each(gamesList, function() {
+    $(".all-games-container").append(
+    `<div class="game">
+    <h3>${this.name}</h3>
+    <div class="img-container"><img src="${this.img}"></div>
+    <ul>
+    <li>Player Count: ${this.minPlayers}-${this.maxPlayers}</li>
+    <li>Play Time: ${this.playTime} minutes</li>
+    <li>Genre: ${this.genre}</li>
+    </ul>
+    <p>${this.description}</p>
+    <hr>
+    </div>`)
+});
 
 $("#favorites").click(function() {
-    $(".favorites-container").slideToggle("slow");
-})
+    $(".favorites-parent-container").slideToggle("slow");
+});
+
+$("#games").click(function() {
+    $(".all-games-parent-container").slideToggle("slow");
+});
+
+$(".nav-dropdown-parent").click(function() {
+    $(".nav-dropdown").toggleClass("hidden");
+});
